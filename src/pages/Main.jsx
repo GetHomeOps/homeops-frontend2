89205d598de7c97f3eb8b422df62fb2d42074688
andres-ordonezs <andresmw.89@gmail.com>
2025-12-22 00:39:57 -0500

    first commit

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -23,7 +23,7 @@ function Main() {
 
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[96rem] mx-auto">
-            <h1>Welcome {currentUser.fullName} !</h1>
+            <h1>Welcome {currentUser?.fullName || "User"} !</h1>
           </div>
         </main>
       </div>
