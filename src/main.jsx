6042c21f55ea8d1ec30ad7d5cc635bd575c8a498
andres-ordonezs <andresmw.89@gmail.com>
2025-12-31 17:38:56 -0800

    HashRouter implemented

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter as Router} from "react-router-dom";
 import ThemeProvider from "./utils/ThemeContext";
@@ -12,5 +12,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
       </ThemeProvider>
     </Router>
+  </React.StrictMode>
+); */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {HashRouter} from "react-router-dom";
+import ThemeProvider from "./utils/ThemeContext";
+import App from "./App";
+import "./i18n";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </HashRouter>
   </React.StrictMode>
 );
